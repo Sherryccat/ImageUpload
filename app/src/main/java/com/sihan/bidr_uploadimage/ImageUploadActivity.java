33f2b34252e0c19ace794cc3849a381707b3f873
Zhongjie Shen <shenzj1994@gmail.com>
2017-10-25 22:39:08 -0700

    Remove unused dependencies. Increase TargetSDK to Oreo.

--- app/src/main/java/com/sihan/bidr_uploadimage/ImageUploadActivity.java
+++ app/src/main/java/com/sihan/bidr_uploadimage/ImageUploadActivity.java
@@ -3,8 +3,6 @@ package com.sihan.bidr_uploadimage;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;

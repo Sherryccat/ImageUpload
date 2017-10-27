5375533f9e71d36aa98ea7a52f73da55012fe8b5
Sihan Bo <ccat-a@hotmail.com>
2017-10-26 17:18:53 -0700

    Hello World

--- app/src/main/java/com/sihan/bidr_uploadimage/ImageUploadActivity.java
+++ app/src/main/java/com/sihan/bidr_uploadimage/ImageUploadActivity.java
@@ -1,19 +1,30 @@
 package com.sihan.bidr_uploadimage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Base64;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class ImageUploadActivity extends AppCompatActivity implements View.OnClickListener
 {
     private static final int result_uploadImage = 1;
-
+    private final String SERVER_ITEM_ADDRESS = "http://52.237.16.136/item_api.php";
     ImageView itemImage;
     android.widget.Button upLoadImageButton;
     EditText itemName;
@@ -23,9 +34,9 @@ public class ImageUploadActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_upload);
 
-        itemImage = (ImageView)findViewById(R.id.itemImage);
-        upLoadImageButton = (Button)findViewById(R.id.uploadImage);
-        itemName = (EditText)findViewById(R.id.itemName);
+        itemImage = findViewById(R.id.itemImage);
+        upLoadImageButton = findViewById(R.id.uploadImage);
+        itemName = findViewById(R.id.itemName);
 
         itemImage.setOnClickListener(this);
         upLoadImageButton.setOnClickListener(this);
@@ -40,7 +51,8 @@ public class ImageUploadActivity extends AppCompatActivity implements View.OnCli
                 break;
 
             case R.id.uploadImage:
-
+                Bitmap image = ((BitmapDrawable)itemImage.getDrawable()).getBitmap();
+                new postItem(image, itemName.getText().toString()).execute();
                 break;
         }
     }
@@ -54,4 +66,70 @@ public class ImageUploadActivity extends AppCompatActivity implements View.OnCli
             itemImage.setImageURI(selectedImage);
         }
     }
+
+    private class postItem extends AsyncTask<Void, Void, Void>{
+
+
+        Bitmap image;
+        String name;
+
+        postItem(Bitmap image, String name ){
+            this.image = image;
+            this.name = name;
+        }
+
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(),Base64.DEFAULT);
+
+            ArrayMap<String,String> dataToSend = new ArrayMap<>();
+            dataToSend.put("image",encodedImage);
+            dataToSend.put("name",name);
+
+            //TODO create a variable to hold timeOutParameter
+
+
+            //TODO add url
+
+            try {
+                HttpURLConnection httpPost = new HttpURLConnection(new URL(SERVER_ITEM_ADDRESS)) {
+                    @Override
+                    public void disconnect() {
+
+                    }
+
+                    @Override
+                    public boolean usingProxy() {
+                        return false;
+                    }
+
+                    @Override
+                    public void connect() throws IOException {
+
+                    }
+                };
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
+    /* TODO create httpParam to get timeout info
+    private HttpURLConnection instead
+    */
+
+    private class
 }
